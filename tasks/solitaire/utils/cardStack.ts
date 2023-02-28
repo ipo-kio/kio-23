3750ae51c4ac56ea52d42d9468bb76258009ba92
kyzinatra <main@kyzinatra.ru>
2023-02-28 22:18:48 +0300

    config fix and my try to do stm with it

---
+++ tasks/solitaire/utils/cardStack.ts
@@ -0,0 +1,24 @@
+import { CARD_VALUES, MAX_CARD_VALUE } from "../constants/card";
+import { TCell, TGrid } from "../types/card";
+
+export function getStackById(grid: TGrid, id: string | number): null | TCell {
+  const cell = grid.find(cell => cell.some(card => card.key === id));
+  if (!cell) return null;
+  const keyIndex = cell.findIndex(card => card.key === id);
+  return [...cell].slice(keyIndex);
+}
+
+export function hasFullStack(cell: TCell): number {
+  let stackCount = 0;
+  let res = -1;
+  cell.forEach((card, i) => {
+    if (CARD_VALUES[card.title] === MAX_CARD_VALUE - stackCount) stackCount += 1;
+    else return (stackCount = +(CARD_VALUES[card.title] === MAX_CARD_VALUE));
+    if (stackCount === MAX_CARD_VALUE + 1) return (res = i - MAX_CARD_VALUE);
+  });
+  return res;
+}
+
+export function getProgress(grid: TGrid) {
+  return grid.reduce((l, cell) => l + cell.length, 0);
+}
